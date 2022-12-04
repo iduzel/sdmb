da724269bbee7a283239a347c8c995f90715f937
Ismail_Duzel <iduzel49@gmail.com>
2022-12-04 20:01:31 +0100

    bs2

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react';
+import DrawerAppBar from '../Appbar/Appbar';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <DrawerAppBar />
+  )
+}
+
+export default Header
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <Box className='home bg-dark text-danger'>Home</Box>
+  )
+}
+
+export default Home
